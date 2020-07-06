cd664560d21a2b44a1cda46882b96d9952546fc7
Jelle Hockx <jelle@hermespartners.nl>
2020-07-06 15:08:50 +0200

    Add predicted solar average line to plot

--- read_influx.py
+++ read_influx.py
@@ -12,12 +12,19 @@ host = ''
 port = 8086
 username = ''
 password = ''
+predicted_solar = {
+    2020: {1: 98.07, 2: 161.26, 3: 272.15, 4: 325.21, 5: 364.55, 6: 358.59,
+           7: 366.34, 8: 345.47, 9: 284.96, 10: 210.15, 11: 120.13, 12: 73.92}
+}
 
 # Pandas DataFrame results
 client = DataFrameClient(host=host, port=port, username=username, password=password)
+now = datetime.now()
+first_day_of_the_month = pd.to_datetime(date(now.year, now.month, 1))
+last_day_of_the_month = pd.to_datetime(date(now.year, now.month + 1, 1) - timedelta(days=1), utc=True)
 
 
-def get_df_current_month(entity, unit):
+def get_df_current_month(entity, unit, now, last_day_of_the_month):
     # Get daily data
     result = client.query(f"SELECT entity_id, value FROM homeassistant.infinite.{unit} WHERE entity_id = '{entity}' "
                           f"AND time >= now() - 31d")
@@ -25,11 +32,9 @@ def get_df_current_month(entity, unit):
     df = df.sort_index().resample('D').max()
 
     # Filter data this month
-    now = datetime.now()
     df = df[df.index.month == now.month]
 
     # Add empty value on the last day of the month for the plot (if it doesn't exist)
-    last_day_of_the_month = pd.to_datetime(date(now.year, now.month + 1, 1) - timedelta(days=1), utc=True)
     if df.index[-1] != last_day_of_the_month:
         df = df.append(pd.DataFrame(index=[last_day_of_the_month], data=[[entity, 0]], columns=['entity_id', 'value']))
     return df
@@ -37,10 +42,11 @@ def get_df_current_month(entity, unit):
 
 while True:
     print('Start loop...')
+
     # Build traces
-    df = get_df_current_month('daily_electricity_usage_total', 'kWh')
+    df = get_df_current_month('daily_electricity_usage_total', 'kWh', now, last_day_of_the_month)
     trace1 = go.Bar(name='Verbruik totaal', x=df.index, y=df['value'], marker_color='blue')
-    df = get_df_current_month('daily_yield', 'kWh')
+    df = get_df_current_month('daily_yield', 'kWh', now, last_day_of_the_month)
     trace2 = go.Bar(name='Opbrengst', x=df.index, y=df['value'], marker_color='limegreen')
 
     # Plotly build figure
@@ -51,6 +57,15 @@ while True:
         "legend_orientation": "h"
     }
     fig = go.Figure(data=data, layout=layout)
+    fig.add_shape(
+        # Predicted (mean) solar horizontal line
+        type="line",
+        x0=first_day_of_the_month,
+        y0=predicted_solar[now.year][now.month] / last_day_of_the_month.day,
+        x1=last_day_of_the_month,
+        y1=predicted_solar[now.year][now.month] / last_day_of_the_month.day,
+        line={"color": "gray", "width": 4}
+    )
     fig.write_html("./current-month.html", config={'staticPlot': True})
 
     print('End loop...')

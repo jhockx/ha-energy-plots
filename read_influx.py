e3fa6cb229b7f190fe45cd6acafad69ce2676834
Jelle Hockx <jelle@hermespartners.nl>
2020-07-06 21:58:10 +0200

    Add yearly plots

--- read_influx.py
+++ read_influx.py
@@ -1,6 +1,7 @@
 print('Start influx script')
 
-from datetime import datetime, date, timedelta
+import calendar
+from datetime import datetime, date
 from time import sleep
 
 import pandas as pd
@@ -20,8 +21,8 @@ predicted_solar = {
 # Pandas DataFrame results
 client = DataFrameClient(host=host, port=port, username=username, password=password)
 now = datetime.now()
-first_day_of_the_month = pd.to_datetime(date(now.year, now.month, 1))
-last_day_of_the_month = pd.to_datetime(date(now.year, now.month + 1, 1) - timedelta(days=1), utc=True)
+first_day_of_the_month = pd.to_datetime(date(now.year, now.month, 1), utc=True)
+last_day_of_the_month = pd.to_datetime(date(now.year, now.month, calendar.monthrange(now.year, now.month)[1]), utc=True)
 
 
 def get_df_current_month(entity, unit, now, last_day_of_the_month):
@@ -40,9 +41,29 @@ def get_df_current_month(entity, unit, now, last_day_of_the_month):
     return df
 
 
+def get_df_current_year(entity, unit, now):
+    # Get daily data
+    result = client.query(f"SELECT entity_id, value FROM homeassistant.infinite.{unit} WHERE entity_id = '{entity}' "
+                          f"AND time >= now() - 365d")
+    df = result[f'{unit}']
+    df = df.sort_index().resample('D').max()
+    df = df.sort_index().resample('M', kind='period').sum().to_timestamp()  # returns first day on each month
+
+    # Filter data this year
+    end_of_current_year = pd.to_datetime(date(now.year, 12, 1))
+    df = df[df.index.year == now.year]
+    df['entity_id'] = entity
+
+    # # Add empty value on the last day of the month for the plot (if it doesn't exist)
+    if df.index[-1] != end_of_current_year:
+        df = df.append(pd.DataFrame(index=[end_of_current_year], data=[[entity, 0]], columns=['entity_id', 'value']))
+    return df
+
+
 while True:
     print('Start loop...')
 
+    ##### MONTHLY PLOTS #####
     # Build traces
     df = get_df_current_month('daily_electricity_usage_total', 'kWh', now, last_day_of_the_month)
     trace1 = go.Bar(name='Verbruik totaal', x=df.index, y=df['value'], marker_color='blue')
@@ -66,7 +87,27 @@ while True:
         y1=predicted_solar[now.year][now.month] / last_day_of_the_month.day,
         line={"color": "gray", "width": 4}
     )
-    fig.write_html("./current-month.html", config={'staticPlot': True})
+    fig.write_html("./current-month-static.html", config={'staticPlot': True})
+
+    ##### YEARLY PLOTS #####
+    # Build traces
+    df = get_df_current_year('daily_electricity_usage_total', 'kWh', now)
+    trace1 = go.Bar(name='Verbruik totaal', x=df.index, y=df['value'], marker_color='blue')
+    df = get_df_current_year('daily_yield', 'kWh', now)
+    trace2 = go.Bar(name='Opbrengst', x=df.index, y=df['value'], marker_color='limegreen')
+    x_solar_predicted = [pd.to_datetime(date(now.year, month, 1)) for month in predicted_solar[now.year].keys()]
+    y_solar_predicted = list(predicted_solar[now.year].values())
+    trace3 = go.Bar(name='Prognose', x=x_solar_predicted, y=y_solar_predicted, marker_color='gray')
+
+    # Plotly build figure
+    data = [trace1, trace2, trace3]
+    layout = {
+        "yaxis": dict(title='kWh'),
+        "margin": dict(l=0, r=0, t=0, b=1),
+        "legend_orientation": "h"
+    }
+    fig = go.Figure(data=data, layout=layout)
+    fig.write_html("./current-year-static.html", config={'staticPlot': False})
 
     print('End loop...')
     sleep(300)

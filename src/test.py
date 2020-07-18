78ca180606b0ca6e9e2766f44c12149d9a6e3321
Jelle Hockx <jellehockx@gmail.com>
2020-07-18 14:41:06 +0200

    Make settings be "none-able"

--- src/test.py
+++ src/test.py
@@ -1,17 +1,15 @@
 import json
 import sys
-from datetime import datetime, date
+from datetime import datetime
 
 now = datetime.now()
 
 print('--- start test script ---')
 print('Arguments passed to test script:')
 for arg in sys.argv[1:]:
-	print(arg)
-	assert arg != "null", "One of the arguments is empty, check your settings!"
+    print(arg)
+    assert arg != "null", "One of the arguments is empty, check your settings!"
 
-predicted_solar = json.loads(sys.argv[7])
-print(predicted_solar)
-print("predicted_solar current year, current month:")
-print(predicted_solar[str(now.year)][str(now.month)])
+print("Check if statement")
+print(json.loads(sys.argv[7]) if sys.argv[7] != 'none' else 'ELSE')
 print('--- end test script ---')

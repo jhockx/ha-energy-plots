def0f46d58bc58008a347f6d467eb0f664fa70fa
Jelle Hockx <jellehockx@gmail.com>
2020-08-08 12:53:30 +0200

    Add dashed average lines for monthly gas usage plot

--- energy_plots/src/plot_gas.py
+++ energy_plots/src/plot_gas.py
@@ -1,6 +1,6 @@
 import calendar
 import sys
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from time import sleep
 
 import pandas as pd
@@ -14,6 +14,7 @@ port = int(sys.argv[2])
 username = sys.argv[3]
 password = sys.argv[4]
 daily_gas_usage = sys.argv[5] if sys.argv[5] != 'none' else None
+daily_gas_usage_monthly_avg = sys.argv[6].capitalize() == 'True' if sys.argv[6] != 'none' else None
 
 # Pandas DataFrame results
 client = DataFrameClient(host=host, port=port, username=username, password=password)
@@ -46,7 +47,24 @@ while True:
 
     # Build figure
     fig = go.Figure(data=data, layout=layout)
-    fig.update_layout(xaxis_tickformat='%d %b')
+    fig.update_layout(xaxis_tickformat='%d %b',
+                      xaxis={'range': [first_day_of_the_month - timedelta(days=0.5),
+                                       last_day_of_the_month + timedelta(days=0.5)]}
+                      )
+
+    # Add lines for monthly averages
+    if daily_gas_usage_monthly_avg:
+        df = get_df_current_month(client, daily_gas_usage, 'm3', now, last_day_of_the_month)
+        y = df['value'].mean()
+        fig.add_shape(
+            # Predicted (mean) solar horizontal line
+            type="line",
+            x0=first_day_of_the_month - timedelta(days=1),
+            y0=y,
+            x1=last_day_of_the_month + timedelta(days=1),
+            y1=y,
+            line={"color": "blue", "width": 4, "dash": "dash"}
+        )
 
     # Save figure
     fig.write_html("./src/gas-current-month-static.html", config={'staticPlot': True})

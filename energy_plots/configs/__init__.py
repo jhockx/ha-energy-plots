cc81968d2c09a0229c89a49d49c8fd3073e65c66
Jelle Hockx <jellehockx@gmail.com>
2021-05-04 08:58:20 +0200

    Set app config

--- energy_plots/configs/__init__.py
+++ energy_plots/configs/__init__.py
@@ -4,10 +4,16 @@
 class DashConfig:
     # Flask builtin configuration variables:
     # https://flask.palletsprojects.com/en/1.1.x/config/#builtin-configuration-values
-    ENV = 'development'  # Default = production
+    ENV = 'production'  # Default = production
     # DEBUG = False  # Default: True if ENV is 'development', or False otherwise.
     # TESTING = False  # Default: False
     # PROPAGATE_EXCEPTIONS = False  # Default: None. If not set, this is implicitly true if TESTING or DEBUG is enabled.
+
+    LOGGER_LEVEL = 'INFO'
+
+    # Dash dev tools:
+    # https://dash.plotly.com/devtools
+    DEV_TOOLS = True
 
     # App title (used in for example the Navbar and page title)
     TITLE = 'Energy plots'
@@ -17,10 +23,6 @@ class DashConfig:
         {'name': 'Electricity', 'url': '/electricity', 'layout': 'electricity_layout'},
         {'name': 'Gas', 'url': '/gas', 'layout': 'gas_layout'}
     ]
-
-    # Dash dev tools:
-    # https://dash.plotly.com/devtools
-    DEV_TOOLS = True
 
 
 dash_config = DashConfig
